3007daf2f13d88f12e8548fce71fd33331bb51b3
Sara L <wwwsaramilk@gmail.com>
2022-05-17 21:41:01 -0300

    finalizando animacoes

--- js/script.js
+++ js/script.js
@@ -22,3 +22,30 @@ var slides = document.querySelectorAll('.slide');
                 currentSlide = i;
             });
         });
+
+        // javascript for image slider autoplay navigation
+        var repeat = function(activeClass){
+            let active = document.getElementsByClassName('active');
+            let i = 1;
+
+            var repeater = () => {
+                setTimeout(function() {
+                    [...active].forEach((activeSlide) => {
+                        activeSlide.classList.remove('active');
+                    });
+
+                    slides[i].classList.add('active');
+                    btns[i].classList.add('active');
+                    i++;
+
+                    if(slides.length == i){
+                        i = 0;
+                    }
+                    if(i >= slides.length){
+                        return;
+                    }
+                    repeater();
+                }, 10000);
+            }
+            repeater();
+        }
